0b97cdacfd87937fcc2878fd4e46a243e08ba073
LS <latifsulistyo.me@gmail.com>
2020-05-29 09:51:47 +0700

    feat: update settings ✨
    - add wsl.conf
    - add shellcheck package
    - update gitconfig for linux & wsl
    - alter `install.sh` and `install.conf.yml`

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ DOTBOT_BIN="bin/dotbot"
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 cd "${BASEDIR}"
-git -C "${DOTBOT_DIR}" submodule sync --quiet --recursive
-git submodule update --init --recursive "${DOTBOT_DIR}"
+git submodule sync --recursive
+git submodule update --init --recursive
 
 "${BASEDIR}/${DOTBOT_DIR}/${DOTBOT_BIN}" -d "${BASEDIR}" -c "${CONFIG}" "${@}"
