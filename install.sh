73ad0d187c9af030a87e6635099b56c204d9ec7e
LS <latifsulistyo.me@gmail.com>
2021-04-25 17:30:58 +0700

    feat: add new scripts and refactor functions ✨

--- install.sh
+++ install.sh
@@ -1,33 +1,26 @@
 #!/usr/bin/env bash
 
-set -eo pipefail
+set -euo pipefail
 
-! (umask | grep -q 022) && umask 022
+export DOTFILES=${DOTFILES:-$HOME/.files}
 
 REPO=${GITHUB_WORKSPACE:-https://github.com/latipun7/dotfiles.git}
 CONFIG="install.conf.yml"
 DOTBOT_DIR=".bot"
 DOTBOT_BIN="bin/dotbot"
 
-if [ -z "$DOTFILES" ]; then
-  DOTDIR="$HOME/.files"
-else
-  DOTDIR="$DOTFILES"
+# shellcheck source=linux/bin/lib/_functions.sh
+source "$DOTFILES/linux/bin/lib/_functions.sh"
+
+if [ ! -d "$DOTFILES" ]; then
+  git clone --recurse-submodules --shallow-submodules -j 7 "$REPO" "$DOTFILES"
+  chmod -R +x "$DOTFILES/install.sh" "$DOTFILES/linux/bin"
 fi
 
-if [ ! -d "$DOTDIR" ]; then
-  git clone --recurse-submodules --shallow-submodules -j 7 "$REPO" "$DOTDIR"
-  chmod -R +x "$DOTDIR/install.sh" "$DOTDIR/linux/bin"
-fi
+git -C "$DOTFILES" submodule sync --recursive
+git -C "$DOTFILES" submodule update --init --recursive
 
-git -C "$DOTDIR" submodule sync --recursive
-git -C "$DOTDIR" submodule update --init --recursive
-
-"$DOTDIR/$DOTBOT_DIR/$DOTBOT_BIN" -d "$DOTDIR" -c "$DOTDIR/$CONFIG" "$@"
-
-function info() {
-  echo -e "\n  \e[1;35m==>\e[0m $1 ℹ\n"
-}
+"$DOTFILES/$DOTBOT_DIR/$DOTBOT_BIN" -d "$DOTFILES" -c "$DOTFILES/$CONFIG" "$@"
 
 if ! hash bootstrap 2>/dev/null; then
   info 'Bootstrapping for the first time ...'
