ac79881969a5efcc59133c154f051e8dd9d4c64d
Latif Sulistyo <latifsulistyo.me@gmail.com>
2021-04-28 02:44:18 +0700

    fix: docker arch missmatch resolved 🐳

--- linux/bin/lib/_apt-packages.sh
+++ linux/bin/lib/_apt-packages.sh
@@ -35,7 +35,7 @@ if [ ! -f /etc/apt/sources.list.d/docker.list ]; then
   step "Install docker"
 
   curl -fsLS https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-  echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" |
+  echo "deb [arch=arm64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" |
     sudo tee /etc/apt/sources.list.d/docker.list &>/dev/null
 
   sudo apt update
