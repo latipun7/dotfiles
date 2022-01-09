9c835451591afc79ed1e9f4f5a8e5b54fbba70f4
Latif Sulistyo <latifsulistyo.me@gmail.com>
2022-01-10 05:22:57 +0700

    feat: add `adminer` 🐘

---
+++ home/www/adminer/index.php
@@ -0,0 +1,38 @@
+<?php
+function adminer_object() {
+    // required to run any plugin
+    include_once "./plugins/plugin.php";
+
+    // autoloader
+    foreach (glob("plugins/*.php") as $filename) {
+        include_once "./$filename";
+    }
+
+    // enable extra drivers just by including them
+    //~ include "./plugins/drivers/simpledb.php";
+
+    $plugins = array(
+        // specify enabled plugins here
+        new AdminerEditCalendar(),
+        new AdminerTinymce(),
+        new AdminerFileUpload("data/"),
+        new AdminerSlugify(),
+        new AdminerEditForeign(),
+        new AdminerForeignSystem(),
+        new AdminerEnumOption(),
+        new AdminerEnumTypes(),
+        new AdminerPrettyJsonColumn(),
+    );
+
+    /* It is possible to combine customization and plugins:
+    class AdminerCustomization extends AdminerPlugin {
+    }
+    return new AdminerCustomization($plugins);
+    */
+
+    return new AdminerPlugin($plugins);
+}
+
+// include original Adminer or Adminer Editor
+include "./adminer.php";
+?>
