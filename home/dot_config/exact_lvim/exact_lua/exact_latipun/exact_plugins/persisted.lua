cdfa5a5db216d826ea08cae36a1a254fc0bfe1b9
Latif Sulistyo <latifsulistyo.me@gmail.com>
2022-08-23 08:39:55 +0700

    fix(lvim): resolve olimorris/persisted.nvim#24

--- home/dot_config/exact_lvim/exact_lua/exact_latipun/exact_plugins/persisted.lua
+++ home/dot_config/exact_lvim/exact_lua/exact_latipun/exact_plugins/persisted.lua
@@ -22,14 +22,26 @@ M.config = function()
     end,
     telescope = {
       before_source = function()
-        vim.api.nvim_input("<Esc><Cmd>%bd<CR>")
+        local status_ok, _ = pcall(require, "bufdelete")
+        if status_ok then
+          vim.api.nvim_input("<Esc><Cmd>bufdo Bdelete<CR>")
+        else
+          vim.api.nvim_input("<Esc><Cmd>%bd<CR>")
+        end
       end,
       after_source = function(session)
-        vim.notify(
-          "Loaded session " .. session.name,
-          vim.log.levels.INFO,
-          { title = title }
-        )
+        vim.defer_fn(function()
+          local status_ok, _ = pcall(require, "notify")
+          if status_ok then
+            vim.notify(
+              "Loaded session " .. session.name,
+              vim.log.levels.INFO,
+              { title = title }
+            )
+          else
+            print("Loaded session " .. session.name)
+          end
+        end, 0)
       end,
     },
   })
