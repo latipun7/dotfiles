d90dddca7a6f1558ffb3281829d8bf3031c5c9aa
Latif Sulistyo <latifsulistyo.me@gmail.com>
2022-10-01 14:14:33 +0700

    feat(lvim): tweaks nvim notify 🔔

--- home/dot_config/exact_lvim/exact_lua/exact_latipun/lvim_builtin.lua
+++ home/dot_config/exact_lvim/exact_lua/exact_latipun/lvim_builtin.lua
@@ -155,6 +155,29 @@ M.config = function()
     "Makefile",
     "package.json",
   }
+
+  -- ▖ ▖  ▗ ▘▐▘
+  -- ▛▖▌▛▌▜▘▌▜▘▌▌
+  -- ▌▝▌▙▌▐▖▌▐ ▙▌
+  --           ▄▌
+
+  lvim.builtin.notify.opts.min_width = function()
+    return math.floor(vim.o.columns * 0.4)
+  end
+  lvim.builtin.notify.opts.max_width = function()
+    return math.floor(vim.o.columns * 0.4)
+  end
+  lvim.builtin.notify.opts.max_height = function()
+    return math.floor(vim.o.lines * 0.8)
+  end
+  lvim.builtin.notify.opts.render = function(...)
+    local notif = select(2, ...)
+    local style = notif.title[1] == "" and "minimal" or "default"
+    require("notify.render")[style](...)
+  end
+  lvim.builtin.notify.opts.stages = "fade_in_slide_out"
+  lvim.builtin.notify.opts.timeout = 3000
+  lvim.builtin.notify.opts.background_colour = "NormalFloat"
 end
 
 return M
