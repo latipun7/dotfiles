b29c446dd26ba37e42e9f6ea57476199ef967979
Latif Sulistyo <latifsulistyo.me@gmail.com>
2022-10-08 09:06:00 +0700

    feat(lvim): install noice.nvim 👍

--- home/dot_config/exact_lvim/exact_lua/exact_latipun/exact_plugins/init.lua
+++ home/dot_config/exact_lvim/exact_lua/exact_latipun/exact_plugins/init.lua
@@ -85,6 +85,12 @@ M.config = function()
       requires = { "MunifTanjim/nui.nvim" },
       config = function() require("latipun.plugins.neotree").config() end,
     },
+    {
+      "folke/noice.nvim",
+      event = "VimEnter",
+      requires = { "MunifTanjim/nui.nvim", "rcarriga/nvim-notify" },
+      config = function() require("latipun.plugins.noice").config() end,
+    },
     { "famiu/bufdelete.nvim", event = "BufReadPre" },
     { "hrsh7th/cmp-cmdline", event = "CmdlineEnter" },
     { "alker0/chezmoi.vim" },
---
+++ home/dot_config/exact_lvim/exact_lua/exact_latipun/exact_plugins/noice.lua
@@ -0,0 +1,58 @@
+local M = {}
+
+M.config = function()
+  local status_ok, noice = pcall(require, "noice")
+  if not status_ok then return end
+
+  noice.setup({
+    cmdline = {
+      view = "cmdline_popup",
+    },
+    popupmenu = {
+      enabled = false,
+    },
+    notify = {
+      enabled = lvim.builtin.notify.active,
+    },
+    routes = {
+      {
+        filter = { event = "msg_show", kind = "search_count" },
+        opts = { skip = true },
+      },
+      {
+        view = "split",
+        filter = { event = "msg_show", min_height = 20 },
+      },
+      {
+        filter = { event = "msg_show", find = "E486:" },
+        opts = { skip = true },
+      },
+      {
+        filter = { event = "msg_show", find = "; before #" },
+        opts = { skip = true },
+      },
+      {
+        filter = { event = "msg_show", find = "; after #" },
+        opts = { skip = true },
+      },
+      {
+        filter = { event = "msg_show", find = " lines, " },
+        opts = { skip = true },
+      },
+      {
+        filter = { event = "msg_show", find = "go up one level" },
+        opts = { skip = true },
+      },
+      {
+        filter = { event = "msg_show", find = "yanked" },
+        opts = { skip = true },
+      },
+      {
+        filter = { find = "No active Snippet" },
+        opts = { skip = true },
+      },
+    },
+  })
+end
+
+return M
