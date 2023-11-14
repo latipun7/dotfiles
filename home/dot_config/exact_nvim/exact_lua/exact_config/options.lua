2b653795acfa954919757c0a4bd9e235630eedbf
Latif Sulistyo <latifsulistyo.me@gmail.com>
2023-11-14 22:18:08 +0700

    feat(nvim): add spellcheck

--- home/dot_config/exact_nvim/exact_lua/exact_config/options.lua
+++ home/dot_config/exact_nvim/exact_lua/exact_config/options.lua
@@ -1,3 +1,11 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+local opt = vim.opt
+
+opt.spell = true
+opt.spelllang = { "en", "id", "cjk" }
+opt.spelloptions = { "camel" }
+opt.spellcapcheck = ""
+opt.spellfile = { vim.fn.expand(vim.fn.stdpath("config") .. "/spell/mix.utf-8.add") }
