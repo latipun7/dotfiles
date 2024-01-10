9c0fa520822b59e0039ea238bcf952d2bca8c975
Latif Sulistyo <latifsulistyo.me@gmail.com>
2024-01-10 07:52:22 +0700

    fix(nvim): remove persisted session name workaround

--- home/dot_config/exact_nvim/exact_lua/exact_plugins/persisted.lua
+++ home/dot_config/exact_nvim/exact_lua/exact_plugins/persisted.lua
@@ -1,38 +1,6 @@
----@see source https://github.com/olimorris/persisted.nvim/blob/315cd1a8a501ca8e0c1d55f0c245b9cc0e1ffa01/lua/persisted/init.lua#L220
-local function parse_session_file(session)
-  local utils = require("utils")
-  local config = require("persisted.config")
-  local save_dir = config.options.save_dir
-  local branch_separator = config.options.branch_separator
-  local dir_separator = require("persisted.utils").get_dir_pattern()
-  local session_name = utils.escape_pattern(session, save_dir, ""):gsub("%%", dir_separator):sub(1, -5)
-
-  if vim.fn.has("win32") == 1 then
-    session_name = utils.escape_pattern(session_name, dir_separator, ":", 1)
-    session_name = utils.escape_pattern(session_name, dir_separator, "\\")
-  end
-
-  local branch, dir_path
-
-  if string.find(session_name, branch_separator, 1, true) then
-    local splits = vim.split(session_name, branch_separator, { plain = true })
-    branch = table.remove(splits, #splits)
-    dir_path = vim.fn.join(splits, branch_separator)
-  else
-    dir_path = session_name
-  end
-
-  return {
-    ["name"] = session_name,
-    ["file_path"] = session,
-    ["branch"] = branch,
-    ["dir_path"] = dir_path,
-  }
-end
-
 local function format_display(session_data)
   local str
-  if session_data.branch then
+  if session_data.branch ~= "" then
     str = string.format("%s (branch: %s)", session_data.dir_path, session_data.branch)
   else
     str = session_data.dir_path
@@ -82,8 +50,7 @@ return {
         pattern = "PersistedLoadPost",
         group = persisted_hooks_group,
         callback = function(session)
-          local session_data = parse_session_file(session.data)
-          local str = format_display(session_data)
+          local str = format_display(session.data)
           vim.notify("Loaded session " .. str, vim.log.levels.INFO, { title = title })
         end,
       })
