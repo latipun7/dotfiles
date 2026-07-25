5a26035ce423aea70971de128207b9c6db1d93ff
Latif Sulistyo <latifsulistyo.me@gmail.com>
2026-07-26 05:19:50 +0700

    feat(nvim): allow trailing commas in jsonc files

--- home/dot_config/exact_nvim/exact_lua/exact_plugins/lsp.lua
+++ home/dot_config/exact_nvim/exact_lua/exact_plugins/lsp.lua
@@ -40,6 +40,18 @@ return {
           -- IMPORTANT: this is required for taplo LSP to work in non-git repositories
           root_dir = require("lspconfig.util").root_pattern("*.toml", ".git"),
         },
+        jsonls = {
+          settings = {
+            json = {
+              schemas = {
+                {
+                  fileMatch = { "*.jsonc" },
+                  schema = { allowTrailingCommas = true },
+                },
+              },
+            },
+          },
+        },
       },
     },
   },
