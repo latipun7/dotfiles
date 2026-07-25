3300fa606e832f1e9306e6a18855625dbd425993
Latif Sulistyo <latifsulistyo.me@gmail.com>
2026-07-26 05:19:04 +0700

    feat(nvim): configure snacks.nvim float terminal

---
+++ home/dot_config/exact_nvim/exact_lua/exact_plugins/terminal.lua
@@ -0,0 +1,14 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    terminal = {
+      win = {
+        position = "float",
+        border = "rounded",
+        height = 0.9,
+        width = 0.9,
+        zindex = 50,
+      },
+    },
+  },
+}
