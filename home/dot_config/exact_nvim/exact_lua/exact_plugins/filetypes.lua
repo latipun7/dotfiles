f64b70ed48fa3e6410c6b312bb0941ac52f6044a
Latif Sulistyo <latifsulistyo.me@gmail.com>
2026-09-09 08:31:56 +0700

    feat(nvim): add typst integration

--- home/dot_config/exact_nvim/exact_lua/exact_plugins/filetypes.lua
+++ home/dot_config/exact_nvim/exact_lua/exact_plugins/filetypes.lua
@@ -8,4 +8,18 @@ return {
     enabled = (vim.fn.executable("cargo") == 1),
     build = "cargo build --release",
   },
+  {
+    "chomosuke/typst-preview.nvim",
+    opts = function()
+      local is_windows = vim.fn.has("win32") == 1
+      local tinymist_bin = "tinymist"
+      local tinymist_path = is_windows and (tinymist_bin .. ".cmd") or tinymist_bin
+
+      return {
+        dependencies_bin = {
+          ["tinymist"] = tinymist_path,
+        },
+      }
+    end,
+  },
 }
